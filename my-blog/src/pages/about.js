61d49deac962e75cc3e4b59cb75313a6f259cb9f
eduardonk9999 <thiago.novak7@gmail.com>
2020-12-13 13:57:28 -0300

    Add, links

--- my-blog/src/pages/about.js
+++ my-blog/src/pages/about.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import Layout from "../components/layout"
 
 const AboutPag = () => (
-  <h1>About Page</h1>
+  <Layout>
+    <h1>About Page</h1>
+      <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/" activeStyle={{ color: 'red' }}>Home</Link>
+          </li>
+        </ul>
+  </Layout>
+  
 )
 
 export default AboutPag;
