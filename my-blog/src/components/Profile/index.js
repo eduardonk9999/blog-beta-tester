eba3ff59e4689129911bf8d0cd50bcbd47439659
eduardonk9999 <thiago.novak7@gmail.com>
2020-12-18 11:54:57 -0300

    Add, components

---
+++ my-blog/src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Eduardo</h1>
+    <h2>Front-End</h2>
+    <p>Programador Front-end, e outras coisas mais.</p>
+  </div>
+)
+
+export default Profile;
